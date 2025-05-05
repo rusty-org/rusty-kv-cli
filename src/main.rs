25c571960e42430046358d015caf5ea1079c6ee3
devpiush <dev.bosepiush@gmail.com>
2025-05-05 16:22:43 +0530

    code: working on maintaning the various commands

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,57 @@
-use tokio::net::TcpListener;
 
+use tokio::net::{TcpListener, TcpStream};
+
+use std::io;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 
 #[tokio::main]
 async fn main() {
   let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
-
   loop {
     let stream = listener.accept().await;
-
     match stream {
-      Ok((mut stream, _)) => {
-
+      Ok((stream, _)) => {
         tokio::spawn(async move {
-          let mut buf = [0; 512];
-
-          loop {
-            let read_count = stream.read(&mut buf).await.unwrap();
-
-            if read_count == 0 {
-              break;
-            }
-
-            stream.write(b"+PONG\r\n").await.unwrap();
-          }
+          accept_connection(stream).await.unwrap();
         });
       }
-
       Err(e) => {
         println!("error: {}", e);
+      }
+    }
+  }
+}
+
+async fn accept_connection(mut socket: TcpStream) -> io::Result<()> {
+  loop {
+    let mut buffer = [0; 1024];
+    match socket.read(&mut buffer).await {
+      Ok(0) => {
+        println!("Connection closed");
+        return Ok(());
+      }
+      Ok(n) => {
+        println!("Accepted connection");
+        let request = String::from_utf8_lossy(&buffer[..n]);
+        if request.contains("PING") {
+
+          socket.write_all(b"+PONG\r\n").await?;
+
+        } else if request.contains("ECHO") {
+
+          let response = format!("{}\r\n", request.trim_start_matches("ECHO "));
+          socket.write_all(response.as_bytes()).await?;
+
+        } else {
+
+          println!("Received: {}", request);
+          socket.write_all(b"-ERR unknown command\r\n").await?;
+
+        }
+      }
+      Err(e) => {
+        eprintln!("Error reading from socket: {}", e);
+        return Err(e);
       }
     }
   }
