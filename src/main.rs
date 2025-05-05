7423da6b8b76244f114795838dd480aceb75a084
devpiush <dev.bosepiush@gmail.com>
2025-05-05 14:16:42 +0530

    code: respond to multiple ping commands asynchronously

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+use tokio::net::TcpListener;
+
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+
+#[tokio::main]
+async fn main() {
+  let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+
+  loop {
+    let stream = listener.accept().await;
+
+    match stream {
+      Ok((mut stream, _)) => {
+
+        tokio::spawn(async move {
+          let mut buf = [0; 512];
+
+          loop {
+            let read_count = stream.read(&mut buf).await.unwrap();
+
+            if read_count == 0 {
+              break;
+            }
+
+            stream.write(b"+PONG\r\n").await.unwrap();
+          }
+        });
+      }
+
+      Err(e) => {
+        println!("error: {}", e);
+      }
+    }
+  }
+}
